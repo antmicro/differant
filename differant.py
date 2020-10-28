d21699a865a0af9f7965f8617f65cb2b1cec8042
Michael Gielda <mgielda@antmicro.com>
2020-10-28 06:59:38 +0100

    Implement file watching vol 1

--- differant.py
+++ differant.py
@@ -2,18 +2,10 @@
 
 # for now assuming the 'derived' repo is just checked out
 
-import fire, yaml, git, os, shutil, sys, pathlib, subprocess, unidiff
+import fire, yaml, git, os, shutil, sys, pathlib, subprocess, unidiff, time, logging
 
-def dirdiff(directory, override=False):
+def dirdiff(directory: str):
     "Either a function to diff directories, or a city in Northwest England, close to Dirham and Dirwick."
-    if not shutil.rmtree.avoids_symlink_attacks:
-        print("Sorry, but your system is susceptible to symlink attacks. Consider switching. Exiting.")
-        sys.exit(1)
-    directory = directory.rstrip('/')
-    abspath = os.path.abspath(directory)
-    if abspath in pathlib.Path(os.getcwd()).parents or abspath == os.getcwd():
-        print("Please do not call dirdiff from inside of the target directory.")
-        sys.exit(1)
 
     f = open(directory + '/.differant.yml', 'r')
     conf = yaml.safe_load(f)
@@ -22,9 +14,6 @@ def dirdiff(directory, override=False):
     print(conf)
     upstream = directory + '-upstream'
     derived = directory + '-derived'
-    if override == True:
-        shutil.rmtree(upstream)
-        shutil.rmtree(derived)
     if not os.path.isdir(upstream):
         repo = git.Repo.clone_from(conf['upstream'], upstream, depth=1, branch=conf['tag'])
     else:
@@ -99,5 +88,41 @@ def check_if_refactor(patch, refactors):
     patch.only_refactors = True
     return True
 
-if __name__ == '__main__':
-    fire.Fire(dirdiff)
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+class DiffingHandler(FileSystemEventHandler):
+    def __init__(self, directory):
+        self.directory = directory
+    def on_modified(self, event):
+        if event.src_path == self.directory + "/.differant.yml":
+            dirdiff(event.src_parh)
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+
+def watch(directory: str, override: bool = False):
+    if not shutil.rmtree.avoids_symlink_attacks:
+        print("Sorry, but your system is susceptible to symlink attacks. Consider switching. Exiting.")
+        sys.exit(1)
+    directory = directory.rstrip('/')
+    abspath = os.path.abspath(directory)
+    if abspath in pathlib.Path(os.getcwd()).parents or abspath == os.getcwd():
+        print("Please do not call dirdiff from inside of the target directory.")
+        sys.exit(1)
+    upstream = directory + '-upstream'
+    derived = directory + '-derived'
+    if override == True:
+        shutil.rmtree(upstream)
+        shutil.rmtree(derived)
+    observer = Observer()
+    observer.schedule(DiffingHandler(directory), path=directory, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
+
+if __name__ == "__main__":
+
+    fire.Fire(watch)
